a9d4e94cdc686a191d173769a7b1f4fd06df2a21
Dan Foley <dan@dan-foley.com>
2020-02-04 23:49:40 +0000

    Done.

--- lib/author.rb
+++ lib/author.rb
@@ -1,1 +1,30 @@
 require "pry"
+
+class Author
+  
+  attr_accessor :name
+
+  def initialize(name)
+    @name  = name
+  end
+  
+  def posts
+    Post.all
+  end
+  
+  def add_post(post)
+    post.author = self
+  end
+  
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+    # Post.all << post
+  end
+  
+  def self.post_count
+    # binding.pry
+    Post.all.count
+  end
+
+end
